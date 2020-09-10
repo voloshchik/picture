45164e6dc19b43a23c4d42c801322b1d263470f4
vladimir <lovkiy2012@gmail.com>
2020-09-10 20:23:18 +0300

    24. Implementation of Drag & Drop image loading

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
 import scrolling from './modules/scrolling';
+import drop from './modules/drop';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
@@ -26,5 +27,6 @@ window.addEventListener('DOMContentLoaded', () => {
     accordion('.accordion-heading');
     burger('.burger-menu', '.burger');
     scrolling('.pageup');
-    console.log('dddddddddddd');
+    drop();
+    console.log('dddfdee33');
 });
